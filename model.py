57dcab671fb1bce29504724e55db8b96558640cb
Israel Bittencourt Cannavó <95496676+icannavo@users.noreply.github.com>
2022-08-03 09:15:15 -0300

    Api filmes Rest CURD_with_FASTAPI
    Api filmes Rest CURD_with_FASTAPI

---
+++ model.py
@@ -0,0 +1,17 @@
+from sqlalchemy import Boolean, Column, Integer, String
+from db_handler import Base
+
+
+class Movies(Base):
+    """
+    This is a model class. which is having the movie table structure with all the constraint
+    """
+    __tablename__ = "movie"
+    id = Column(Integer, primary_key=True, autoincrement=True, index=True, nullable=False)
+    movie_id = Column(String, unique=True, index=True, nullable=False)
+    movie_name = Column(String(255), index=True, nullable=False)
+    director = Column(String(100), index=True, nullable=False)
+    geners = Column(String, index=True, nullable=False)
+    membership_required = Column(Boolean, nullable=False, default=True)
+    cast = Column(String(255), index=True, nullable=False)
+    streaming_platform = Column(String, index=True)
---
+++ schema.py
@@ -0,0 +1,43 @@
+from typing import Optional
+from pydantic import BaseModel
+
+
+class MovieBase(BaseModel):
+    movie_name: str
+    director: str
+    geners: str
+    cast: str
+
+
+class MovieAdd(MovieBase):
+    movie_id: str
+    # Optional[str] is just a shorthand or alias for Union[str, None].
+    # It exists mostly as a convenience to help function signatures look a little cleaner.
+    streaming_platform: Optional[str] = None
+    membership_required: bool
+
+    # Behaviour of pydantic can be controlled via the Config class on a model
+    # to support models that map to ORM objects. Config property orm_mode must be set to True
+    class Config:
+        orm_mode = True
+
+
+class Movie(MovieAdd):
+    id: int
+
+    # Behaviour of pydantic can be controlled via the Config class on a model
+    # to support models that map to ORM objects. Config property orm_mode must be set to True
+    class Config:
+        orm_mode = True
+
+
+class UpdateMovie(BaseModel):
+    # Optional[str] is just a shorthand or alias for Union[str, None].
+    # It exists mostly as a convenience to help function signatures look a little cleaner.
+    streaming_platform: Optional[str] = None
+    membership_required: bool
+
+    # Behaviour of pydantic can be controlled via the Config class on a model
+    # to support models that map to ORM objects. Config property orm_mode must be set to True
+    class Config:
+        orm_mode = True
